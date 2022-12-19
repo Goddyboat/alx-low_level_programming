b4dc64c13e15d64afd4a453ca6fe024da85646f9
Goddyboat <godfredboateng009@gmail.com>
2022-12-19 13:54:34 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * resert_to_98 - function that takes a pointer to an int as parameter and
+ * updates the value it points to to 98.
+ *
+ * @n: This is the entry
+ *
+ */
+
+void resert_to_98(int *n)
+{
+	*n = 98;
+}
